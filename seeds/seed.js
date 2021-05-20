1fd03b817e53a3cfb20e0dc5a1408ec58d52b1e6
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-05-20 19:57:41 -0400

    Move userData.js module under seed.js and add individual hooks property

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,14 +1,28 @@
-const seedUsers = require('./userSeeds');
+// Import User model
+const { User } = require('../models');
 
 const sequelize = require('../config/connection');
 
+// Import User seed data
+const userData = require('./userData.json');
+
 const seedAll = async () => {
+
+  // Drop existing tables
   await sequelize.sync({ force: true });
+
   console.log('\n----- DATABASE SYNCED -----\n');
-  await seedUsers();
+
+  // Create user seed data
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true
+  });
+
   console.log('\n----- USERS SEEDED -----\n');
 
   process.exit(0);
 };
 
+// Call seed function declared above
 seedAll();
