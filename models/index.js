7d8069e4d6f48bf5995b374230503e53a70e2393
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-05-20 21:17:35 -0400

    Create model relationships for joins

--- models/index.js
+++ models/index.js
@@ -7,5 +7,34 @@ const Patient = require("./Patient");
 
 // Relationships
 
+// Associating Patient and Note
+Patient.hasMany(Note, {
+    foreignKey: "patient_id",
+    // If patient is deleted, the associated notes should be deleted as well
+    onDelete: "CASCADE"
+})
+
+Note.belongsTo(Patient, {
+    foreignKey: "patient_id"
+})
+
+// Associating User and Patient
+User.hasMany(Patient, {
+    foreignKey: "doctor_id"
+})
+
+Patient.belongsTo(User, {
+    foreignKey: "doctor_id"
+})
+
+// Associating User and Note
+User.hasMany(Note, {
+    foreignKey: "user_id"
+})
+
+Note.belongsTo(User, {
+    foreignKey: "user_id"
+})
+
 // Export the modules
 module.exports = { User, Note, Patient };
