8708a35d433824ac34405debc44cb86f62ce4912
Brandon Ford <Brandon.ford.617@gmail.com>
2021-05-21 17:23:25 -0400

    changed seed file names to data

--- seeds/index.js
+++ seeds/index.js
@@ -1,8 +1,8 @@
-const seedUsers = require('./userSeeds.json');
+const usersData = require('./userData.json');
 
-const seedPatients = require('./patientSeeds.json');
+const patientsData = require('./patientData.json');
 
-const seedRecords = require('./recordSeeds.json');
+const recordsData = require('./recordData.json');
 
 const sequelize = require('../config/connection');
 const { User, Patient, Record } = require('../models');
@@ -11,19 +11,19 @@ const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
 
-  await User.bulkCreate(seedUsers, {
+  await User.bulkCreate(usersData, {
     individualHooks: true,
     returning: true,
   });
   console.log('\n----- Users SEEDED -----\n');
 
-  await Patient.bulkCreate(seedPatients, {
+  await Patient.bulkCreate(patientsData, {
     individualHooks: true,
     returning: true,
   });
   console.log('\n----- Patients SEEDED -----\n');
 
-  await Record.bulkCreate(seedRecords, {
+  await Record.bulkCreate(recordsData, {
     individualHooks: true,
     returning: true,
   });
