dbd40064bc9dba061d549f9dfd4b8f16d2da419d
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-05-20 20:43:07 -0400

    Add Note + Patient model imports

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
-// An index file to gather the models and export them for use
+// An index file to gather the models, establish their relationships and export them for use
 
-// User model
+// Import models
 const User = require('./User');
+const Note = require("./Note");
+const Patient = require("./Patient");
+
+// Relationships
 
 // Export the modules
-module.exports = { User };
+module.exports = { User, Note, Patient };
