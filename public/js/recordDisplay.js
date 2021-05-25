6002c290ec51afc8d016c93865faf485f5a6e7b0
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-05-25 19:43:56 -0400

    Create logic for searching patient name

--- public/js/recordDisplay.js
+++ public/js/recordDisplay.js
@@ -22,3 +22,47 @@ async function recordDisplayFormHandler(event) {
 }
 
 document.querySelector('#recordDisplay').addEventListener('submit', recordDisplayFormHandler);
+
+// Search bar logic
+
+// Tag search bar
+const searchBar = document.querySelector("#search-bar");
+
+// Creates array of all searchable fields from patient records
+const searchNames = Array.from(document.querySelectorAll(".search.name"));
+
+// Adds a new property .textValue to each item that's lower case for easy comparison with search query
+searchNames.forEach(item => {
+    item.textValue = item.textContent.toLowerCase();
+})
+
+
+const search = (event) => {
+    // Captures string entered into input
+    const query = event.target.value.toLowerCase();
+
+    // Finds list items that match
+    const matchArray = searchNames.filter(item => item.textValue.includes(query));
+
+    // Finds list items that DON'T match
+    const noMatchArray = searchNames.filter(item => !item.textValue.includes(query));
+
+    console.log(matchArray);
+    console.log(noMatchArray);
+
+    // Displays those that do match -- has opposite class to 
+    matchArray.forEach(item => {
+        // References container for each search item
+        item.closest(".userPatientsContainer").classList.add("show");
+        item.closest(".userPatientsContainer").classList.remove("hide");
+    })
+
+    // Hides those that don't match
+    noMatchArray.forEach(item => {
+        item.closest(".userPatientsContainer").classList.add("hide");
+        item.closest(".userPatientsContainer").classList.remove("show");
+    })
+}
+
+// Attach search function to searchBar as event listener for each key press
+searchBar.addEventListener("keyup", search);
